137010e3e74364f7b9cc562c787f0b7cd7abca8a
nice2minjoo <102221871+nice2minjoo@users.noreply.github.com>
2023-05-23 17:33:02 +0900

    token test

--- app.py
+++ app.py
@@ -9,13 +9,6 @@ from PIL import Image
 
 OPENAI_API_KEY = "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"
 KAKAO_API_KEY = "aae47d1d4531de86c0547401b169f52c"
-# certification of openai API key
-openai.api_key = OPENAI_API_KEY
-# create Karlo api instance
-karlo = Karlo(service_key = KAKAO_API_KEY)
-
-# select model GPT 3.5 turbo
-model = "gpt-3.5-turbo"
 
 st.title("smart painter")
 
@@ -23,6 +16,13 @@ with st.form("form"):
     query = st.text_input("Input")
     size = st.selectbox("Size", ["1024x1024","512x512","256x256"])
     submit = st.form_submit_button("Submit")
+
+# certification of openai API key
+openai.api_key = OPENAI_API_KEY
+# create Karlo API instance
+karlo = Karlo(service_key = KAKAO_API_KEY)
+# select model GPT 3.5 turbo
+model = "gpt-3.5-turbo"
 
 # generate image using Karlo
 def t2i(text, batch_size=1):
@@ -42,56 +42,22 @@ def t2i(text, batch_size=1):
     # convert response in json format
     response = json.loads(r.content)
     return response
-
 # Base64 decoding and  convert
 def stringToImage(base64_string, mode='RGBA'):
     imgdata = base64.b64decode(str(base64_string))
     img = Image.open(io.BytesIO(imgdata)).convert(mode)
     return img
-  
+
 # question
 if submit and query:
-    # message
-    messages = [
-        {
-            "role": "system",
-            "content": "Imagine the details appearance of the input. Response it shortly around 20 words."
-        },
-        {
-            "role": "user",
-            "content": query
-        }
-    ]
+    query = "Mars Settlement Architecture: Futuristic, Modular, Sustainable Pods"
 
-    # add user message
-    #messages.append(
-        #{
-            #"role": "user",
-            #"content": "Condense up to 4 outward description to focus on nouns and adjectives separated by ,"
-        #}
-    #)
-
-    # call ChatGPT API
-    with st.spinner("Waiting for ChatGPT..."):
-        response = openai.ChatCompletion.create(
-            model=model,
-            messages=messages
-        )
-
-    answer = response['choices'][0]['message']['content']
-    #print(answer)
-
-    st.write(answer)
+    st.write(query)
 
     with st.spinner("Waiting for Karlo..."):
-        response = t2i(answer, 1)
-        result = stringToImage(response.get("images")[0].get("image"), mode='RGB')
 
-    if size == "1024x1024":
-        user_size = 1024
-    elif size == "512x512":
-        user_size = 512
-    elif size == "256x256":
-        user_size = 256
+        response = t2i(query, 1)
+        result = stringToImage(response.get("images")[0].get("image"), mode='RGB')
+        #result.show()
         
-    st.image(result, width = user_size)
+    st.image(result)
