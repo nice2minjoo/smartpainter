611b149ce8e9eb345cd5b3c64c8178aace5c95c4
nice2minjoo <102221871+nice2minjoo@users.noreply.github.com>
2023-05-24 13:26:11 +0900

    smartpainter release

--- app.py
+++ app.py
@@ -1,64 +1,48 @@
 import streamlit as st
 import openai
-from PyKakao import Karlo
-import requests
-import json
-import io
-import base64
-from PIL import Image
 
-OPENAI_API_KEY = "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"
-KAKAO_API_KEY = "aae47d1d4531de86c0547401b169f52c"
+openai.api_key = st.secrets["api_key"]
 
 st.title("smart painter")
 
 with st.form("form"):
-    query = st.text_input("Input")
-    size = st.selectbox("Size", ["1024x1024","512x512","256x256"])
+    user_input = st.text_input("Input")
+    user_size = st.selectbox("Size", ["1024x1024","512x512","256x256"])
     submit = st.form_submit_button("Submit")
 
-# certification of openai API key
-openai.api_key = OPENAI_API_KEY
-# create Karlo API instance
-karlo = Karlo(service_key = KAKAO_API_KEY)
-# select model GPT 3.5 turbo
-model = "gpt-3.5-turbo"
-
-# generate image using Karlo
-def t2i(text, batch_size=1):
-    r = requests.post(
-        'https://api.kakaobrain.com/v1/inference/karlo/t2i',
-        json = {
-            'prompt': {
-                'text': text,
-                'batch_size': batch_size
-            }
+if submit and user_input:
+    gpt_prompt = [
+        {
+            "role": "system",
+            "content": "Imagine the details appearance of the input. Response it shortly around 20 words."
         },
-        headers = {
-            'Authorization': f'KakaoAK {KAKAO_API_KEY}',
-            'Content-Type': 'application/json'
+        {
+            "role": "user",
+            "content": user_input
         }
-    )
-    # convert response in json format
-    response = json.loads(r.content)
-    return response
-
-# Base64 decoding and  convert
-def stringToImage(base64_string, mode='RGBA'):
-    imgdata = base64.b64decode(str(base64_string))
-    img = Image.open(io.BytesIO(imgdata)).convert(mode)
-    return img
-
-# question
-if submit and query:
-    query = "Mars Settlement Architecture: Futuristic, Modular, Sustainable Pods"
-
-    st.write(query)
-
-    with st.spinner("Waiting for Karlo..."):
-
-        response = t2i(query, 1)
-        result = stringToImage(response.get("images")[0].get("image"), mode='RGB')
-        #result.show()
+    ]
+    
+    with st.spinner("Waiting for ChatGPT..."):
+        gpt_response = openai.ChatCompletion.create(
+            model = "gpt-3.5-turbo",
+            messages = gpt_prompt
+        )
+    
+    finish_reason = gpt_response["choices"][0]["finish_reason"]
+    if finish_reason != "stop":
+        st.write("no more token")
+    
+    else:
+        prompt = gpt_response["choices"][0]["message"]["content"]
+        st.write(prompt)
         
-    st.image(result)
+        with st.spinner("Waiting for DALL-E..."):
+            dalle_response = openai.Image.create(
+                prompt = prompt,
+                size = user_size
+            )
+            
+        st.image(dalle_response["data"][0]["url"])
+    
+elif not user_input and submit:
+    st.write("please try again")
